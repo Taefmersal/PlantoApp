c33b2acdb3d91d4941d53eb5b29ae7ae1697e006
Taef mersal <Taefmrrsal@gmail.com>
2024-10-20 09:06:23 +0300

    Initial Commit

---
+++ PlantoApp/PlantoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantoAppApp.swift
+//  PlantoApp
+//
+//  Created by Taef Omar Mersal on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct PlantoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlantoAppTests/PlantoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlantoAppTests.swift
+//  PlantoAppTests
+//
+//  Created by Taef Omar Mersal on 17/04/1446 AH.
+//
+
+import Testing
+@testable import PlantoApp
+
+struct PlantoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
